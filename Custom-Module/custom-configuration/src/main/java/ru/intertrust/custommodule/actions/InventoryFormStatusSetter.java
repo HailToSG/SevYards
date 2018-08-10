5020d6f2fb8586390be5487f619739501b750b92
HailToSG <iegorzaika@gmail.com>
2018-08-10 11:38:41 +0300

    Logic for inventory statuses was added

---
+++ Custom-Module/custom-configuration/src/main/java/ru/intertrust/custommodule/actions/InventoryFormStatusSetter.java
@@ -0,0 +1,102 @@
+package ru.intertrust.custommodule.actions;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.intertrust.cm.core.business.api.CollectionsService;
+import ru.intertrust.cm.core.business.api.CrudService;
+import ru.intertrust.cm.core.business.api.dto.*;
+import ru.intertrust.cm.core.dao.access.AccessControlService;
+import ru.intertrust.cm.core.dao.access.AccessToken;
+import ru.intertrust.cm.core.dao.api.DomainObjectDao;
+import ru.intertrust.cm.core.gui.api.server.action.ActionHandler;
+import ru.intertrust.cm.core.gui.model.ComponentName;
+import ru.intertrust.cm.core.gui.model.action.ActionContext;
+import ru.intertrust.cm.core.gui.model.action.ActionData;
+import ru.intertrust.cm.core.gui.model.action.SimpleActionData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Класс для автозаполнения поля формы "Инвентаризация"
+ */
+
+@ComponentName("inventory.action")
+public class InventoryFormStatusSetter extends ActionHandler {
+
+    private static final String STATUS_DRAFT = "Draft inventory";
+    private static final String STATUS_FINISHED = "Finished inventory";
+    private static final String FIELD_NAME = "name";
+    private static final String QUERY_STATUS_BY_ID = "select * from status where id = {0}";
+    private static final String QUERY_STATUS_BY_NAME = "select * from status where name = {0}";
+
+    @Autowired
+    private CollectionsService collectionsService;
+
+    @Autowired
+    private CrudService crudService;
+
+    @Autowired
+    private AccessControlService accessControlService;
+
+    @Autowired
+    private DomainObjectDao domainObjectService;
+
+    @Override
+    public ActionData executeAction(ActionContext context) {
+        Id rootObjectId = context.getRootObjectId();
+        SimpleActionData actionData = new SimpleActionData();
+
+        if (rootObjectId != null) {
+            DomainObject rootDomainObject = crudService.find(rootObjectId);
+
+            if (getStatusById(rootDomainObject.getStatus()).equals(STATUS_DRAFT)) {
+                changeStatusForDo(rootDomainObject.getId(), STATUS_FINISHED);
+            }
+            if (getStatusById(rootDomainObject.getStatus()).equals(STATUS_FINISHED)) {
+                changeStatusForDo(rootDomainObject.getId(), STATUS_DRAFT);
+            }
+        }
+
+        return actionData;
+    }
+
+    public String getStatusById(Id status) {
+        List<Value> params = new ArrayList<>();
+        Value statusIdValue = new ReferenceValue(status);
+        params.add(statusIdValue);
+        IdentifiableObjectCollection collection = collectionsService.findCollectionByQuery(QUERY_STATUS_BY_ID, params);
+        if (collection != null && collection.size() > 0) {
+            for (IdentifiableObject O : collection) {
+                return O.getString(FIELD_NAME);
+            }
+        }
+
+        return null;
+    }
+
+    public boolean changeStatusForDo(Id doId, String status) {
+        Id statusDomainObject;
+        statusDomainObject = getStatusByName(status);
+        DomainObject saveDomainObject;
+        AccessToken accessToken;
+
+        if (accessControlService != null && domainObjectService != null) {
+            if (statusDomainObject != null) {
+                accessToken = accessControlService.createSystemAccessToken(getClass().getName());
+                saveDomainObject = domainObjectService.setStatus(doId, statusDomainObject, accessToken);
+                return (saveDomainObject != null);
+            }
+        }
+
+        return false;
+    }
+
+    public Id getStatusByName(String name) {
+        List<Value> params = new ArrayList<>();
+        params.add(new StringValue(name));
+        IdentifiableObjectCollection collection = collectionsService.findCollectionByQuery(QUERY_STATUS_BY_NAME, params);
+        if (collection != null && collection.size() > 0)
+            return collection.get(0).getId();
+        else return null;
+    }
+}
---
+++ Custom-Module/custom-configuration/src/main/java/ru/intertrust/custommodule/actions/InventoryStatusDraftVisibilityChecker.java
@@ -0,0 +1,49 @@
+package ru.intertrust.custommodule.actions;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.intertrust.cm.core.business.api.CollectionsService;
+import ru.intertrust.cm.core.business.api.dto.*;
+import ru.intertrust.cm.core.gui.api.server.action.ActionVisibilityChecker;
+import ru.intertrust.cm.core.gui.api.server.action.ActionVisibilityContext;
+import ru.intertrust.cm.core.gui.model.ComponentName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ComponentName("inventory.status.draft.visible.checker")
+public class InventoryStatusDraftVisibilityChecker implements ActionVisibilityChecker {
+
+
+    private static final String STATUS_FINISHED = "Finished inventory";
+    private static final String FIELD_NAME = "name";
+    private static final String QUERY_STATUS_BY_ID = "select * from status where id = {0}";
+
+    @Autowired
+    private CollectionsService collectionsService;
+
+    @Override
+    public boolean isVisible(ActionVisibilityContext context) {
+
+        if (context.getDomainObject() != null && context.getDomainObject().getStatus() != null) {
+            if (getQueryStatusById(context.getDomainObject().getStatus()).equals(STATUS_FINISHED)) {
+
+                return true;
+            } else return false;
+        }
+
+        return false;
+    }
+
+    public String getQueryStatusById(Id status) {
+        List<Value> params = new ArrayList<>();
+        Value statusIdValue = new ReferenceValue(status);
+        params.add(statusIdValue);
+        IdentifiableObjectCollection collection = collectionsService.findCollectionByQuery(QUERY_STATUS_BY_ID, params);
+        if (collection != null && collection.size() > 0) {
+            for (IdentifiableObject O : collection) {
+                return O.getString(FIELD_NAME);
+            }
+        }
+        return null;
+    }
+}
