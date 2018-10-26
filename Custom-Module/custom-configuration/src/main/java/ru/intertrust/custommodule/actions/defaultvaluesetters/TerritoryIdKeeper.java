7e8a7d115dc3c1d60d72be43b615bc7d1fc29032
ezaika <iegorzaika@gmail.com>
2018-10-26 11:17:54 +0300

    Some changes for inventory collection

--- Custom-Module/custom-configuration/src/main/java/ru/intertrust/custommodule/actions/defaultvaluesetters/TerritoryIdKeeper.java
+++ Custom-Module/custom-configuration/src/main/java/ru/intertrust/custommodule/actions/defaultvaluesetters/TerritoryIdKeeper.java
@@ -3,20 +3,11 @@ package ru.intertrust.custommodule.actions.defaultvaluesetters;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.intertrust.cm.core.business.api.dto.DomainObject;
 import ru.intertrust.cm.core.business.api.dto.Id;
-import ru.intertrust.cm.core.config.ConfigurationExplorer;
-import ru.intertrust.cm.core.config.ConfigurationExplorerImpl;
-import ru.intertrust.cm.core.config.gui.form.FormConfig;
-import ru.intertrust.cm.core.config.gui.form.TabGroupConfig;
 import ru.intertrust.cm.core.dao.api.CurrentUserAccessor;
 import ru.intertrust.cm.core.gui.api.server.action.ActionVisibilityChecker;
 import ru.intertrust.cm.core.gui.api.server.action.ActionVisibilityContext;
 import ru.intertrust.cm.core.gui.model.ComponentName;
 import ru.intertrust.custommodule.actions.constants.CustomModuleConstants;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @ComponentName("territory.id.keeper")
 public class TerritoryIdKeeper implements ActionVisibilityChecker {
